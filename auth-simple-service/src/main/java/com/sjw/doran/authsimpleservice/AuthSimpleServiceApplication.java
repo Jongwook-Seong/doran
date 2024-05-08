2a71989d466a47134bb8a7038277fb69a348de12
Xavier Jongwook Seong <ypwa121@gmail.com>
2024-05-08 22:58:43 +0900

    feat: auth-simple-service 생성

---
+++ auth-simple-service/src/main/java/com/sjw/doran/authsimpleservice/AuthSimpleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sjw.doran.authsimpleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSimpleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSimpleServiceApplication.class, args);
+	}
+
+}
---
+++ auth-simple-service/src/test/java/com/sjw/doran/authsimpleservice/AuthSimpleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjw.doran.authsimpleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSimpleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
