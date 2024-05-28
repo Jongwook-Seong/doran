ccfce8b02bd36a4a6f34086ec772c144ab2c9667
Xavier Jongwook Seong <ypwa121@gmail.com>
2024-05-28 13:49:44 +0900

    conf: 주문 서비스 MapStruct 의존성 설정

---
+++ order-service/src/main/java/com/sjw/doran/orderservice/mapper/OrderMapper.java
@@ -0,0 +1,10 @@
+package com.sjw.doran.orderservice.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper {
+
+    OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
+}
