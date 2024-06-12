35d07f299ff4b9a0fab7251f906ce1eb9a21f60d
Xavier Jongwook Seong <ypwa121@gmail.com>
2024-06-13 02:06:25 +0900

    fix: API 게이트웨이 서비스 서킷브레이커 설정 수정

--- apigateway-service/src/main/java/com/sjw/doran/apigatewayservice/fallback/FallbackController.java
+++ apigateway-service/src/main/java/com/sjw/doran/apigatewayservice/fallback/FallbackController.java
@@ -1,31 +1,29 @@
 package com.sjw.doran.apigatewayservice.fallback;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
 
     @GetMapping("/member")
     public ResponseEntity<String> memberServiceFallback() {
+        log.info("Fallback method for member service is called");
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
                 .body("Member Service is currently unavailable. Please try again later.");
     }
 
     @GetMapping("/order")
     public ResponseEntity<String> orderServiceFallback() {
+        log.info("Fallback method for order service is called");
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
                 .body("Order Service is currently unavailable. Please try again later.");
-    }
-
-    @GetMapping("/item")
-    public ResponseEntity<String> itemServiceFallback() {
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-                .body("Item Service is currently unavailable. Please try again later.");
     }
 
 //    @GetMapping("/auth-simple")
