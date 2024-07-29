bde1f826e37f9a86f4a61ff20da1eedf39c034e1
Xavier Jongwook Seong <ypwa121@gmail.com>
2024-07-29 21:37:55 +0900

    init: 요청데이터 생성 애플리케이션 생성

---
+++ requestdata-generator/src/main/java/com/fastcampus/kafkahandson/requestdatagenerator/RequestdataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.kafkahandson.requestdatagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestdataGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestdataGeneratorApplication.class, args);
+    }
+
+}
---
+++ requestdata-generator/src/test/java/com/fastcampus/kafkahandson/requestdatagenerator/RequestdataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.kafkahandson.requestdatagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestdataGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
